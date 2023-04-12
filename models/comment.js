7fdeafcd6ba97833c4ceef6adaa3ea71de644781
r7qb <r7qb@fresnou.org>
2023-04-12 11:32:53 -0700

    Commit

--- models/comment.js
+++ models/comment.js
@@ -12,8 +12,12 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Comment.hasMany(models.Reply, {
+        as:'replies',
+        foreignKey: 'parent_comment_id'
+      })
     }
-  }
+  };
   Comment.init({
     author_name: DataTypes.STRING,
     body: DataTypes.STRING,
@@ -32,7 +36,12 @@ module.exports = (sequelize, DataTypes) => {
     sequelize,
     modelName: 'Comment',
     timestamps: false,
-    tableName: 'blog_comments'
+    tableName: 'blog_comments',
+    defaultScope: {
+      where: {
+        parent_comment_id: null
+      }
+    }
   });
   return Comment;
 };
